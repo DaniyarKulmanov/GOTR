0cf145cbdf10da14f09d8a2378f7fb2d4d0544b9
daniyar kulmanov <daniyarkulmanov@iMac.local>
2023-07-11 19:24:48 +0600

    main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,32 +1,56 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 )
 
-type Color string
+type Node struct {
+	Previous *Node
+	Data     int
+	Next     *Node
+}
 
-const (
-	ColorBlack  Color = "\u001b[30m"
-	ColorRed          = "\u001b[31m"
-	ColorGreen        = "\u001b[32m"
-	ColorYellow       = "\u001b[33m"
-	ColorBlue         = "\u001b[34m"
-	ColorReset        = "\u001b[0m"
-)
+var tail *Node
 
-func colorize(color Color, message string) {
-	fmt.Println(string(color), message, string(ColorReset))
+func (n *Node) AddNode(data int) {
+	fmt.Println("Inserting node", data)
+	if tail == nil {
+		tail = n
+	}
+	n = tail
+	newNode := Node{n, data, nil}
+	n.Next = &newNode
+	tail = &newNode
+}
+
+func (n *Node) PrintNode() {
+	fmt.Println("Printing nodes")
+	iter := n
+	for iter != nil {
+		fmt.Println(iter.Data)
+		iter = iter.Next
+	}
+}
+
+func (n *Node) DeleteLast() {
+	fmt.Println("Deleting last node", tail.Data)
+	tail = tail.Previous
+	(tail.Next).Previous = nil
+	tail.Next = nil
 }
 
 func main() {
-	useColor := flag.Bool("color", false, "display colorized output")
-	flag.Parse()
-
-	if *useColor {
-		colorize(ColorYellow, "Hello, DigitalOcean!")
-		return
-	}
-	fmt.Println("Hello, DigitalOcean!")
+	newNode := Node{nil, 10, nil}
+	newNode.AddNode(20)
+	newNode.AddNode(30)
+	newNode.AddNode(40)
+	newNode.PrintNode()
+	newNode.DeleteLast()
+	newNode.PrintNode()
+	newNode.AddNode(50)
+	newNode.AddNode(60)
+	newNode.AddNode(70)
+	newNode.PrintNode()
+	newNode.DeleteLast()
+	newNode.PrintNode()
 }
