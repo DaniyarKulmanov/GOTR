372d8d6a9502fd44e09f86336b8c06c52251b22c
daniyar kulmanov <daniyarkulmanov@iMac.local>
2023-07-24 08:02:11 +0600

    binary search

--- GoSearch/cmd/gosearch/gosearch.go
+++ GoSearch/cmd/gosearch/gosearch.go
@@ -1,45 +1,38 @@
 package main
 
 import (
+	"GOTR/03-algorithms/1-search"
+	"GOTR/GoSearch/pkg/crawler"
 	"GOTR/GoSearch/pkg/crawler/spider"
 	"GOTR/GoSearch/pkg/index"
 	"flag"
 	"fmt"
-	"strings"
 )
 
 func main() {
-	var search string
+	var word string
 	var depth int
-	sts := [4]string{
+	var BinaryDocs []crawler.Document
+	sts := [2]string{
 		"https://go.dev/",
 		"https://www.programiz.com/golang/",
-		"https://en.wikipedia.org/wiki/Merge_sort",
-		"https://golangify.com/array",
 	}
 	s := spider.New()
-	flag.StringVar(&search, "s", "", "search links")
+	flag.StringVar(&word, "s", "", "word links")
 	flag.IntVar(&depth, "d", 1, "depth size")
 	flag.Parse()
 
 	for i := range sts {
 		result, err := s.Scan(sts[i], depth)
-		result[0].ID = len(index.Documents) + 1
-
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-
-		if search != "" {
-			if strings.Contains(sts[i], search) {
-				index.Create(result[0])
-			}
-		} else {
-			index.Create(result[0])
-
-		}
+		index.Create(result[0])
 	}
-	fmt.Println(index.MapIndex)
+	if word != "" {
+		BinaryDocs = __search.Binary(index.MapIndex[word], index.Documents)
+	}
 	fmt.Println(index.Documents)
+	fmt.Println(BinaryDocs)
 }
