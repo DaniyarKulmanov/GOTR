61cab4d09a56e31aa356f6a2c3a1daf412bd4e2a
daniyar kulmanov <daniyarkulmanov@iMac.local>
2023-07-26 09:04:41 +0600

    refactor search as func

--- 03-algorithms/1-search/search.go
+++ 03-algorithms/1-search/search.go
@@ -1,10 +1,45 @@
-package __search
+package search
 
 import (
 	"GOTR/GoSearch/pkg/crawler"
+	"GOTR/GoSearch/pkg/crawler/spider"
+	"GOTR/GoSearch/pkg/index"
+	"flag"
+	"fmt"
 )
 
-func Binary(words []int, allDocs []crawler.Document) []crawler.Document {
+func ParseUrl(url [2]string) (docs []crawler.Document) {
+	var word string
+	var depth int
+	var BinaryDocs []crawler.Document
+
+	s := spider.New()
+	flag.StringVar(&word, "s", "", "word links")
+	flag.IntVar(&depth, "d", 1, "depth size")
+	flag.Parse()
+
+	for i := range url {
+		result, err := s.Scan(url[i], depth)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		index.Create(result[0])
+	}
+	if word != "" {
+		BinaryDocs = binary(index.MapIndex[word], index.Documents)
+	}
+
+	if BinaryDocs != nil {
+		docs = BinaryDocs
+	} else {
+		docs = index.Documents
+	}
+
+	return docs
+}
+
+func binary(words []int, allDocs []crawler.Document) []crawler.Document {
 	var Docs []crawler.Document
 	for _, word := range words {
 		low := 0
--- GoSearch/cmd/gosearch/gosearch.go
+++ GoSearch/cmd/gosearch/gosearch.go
@@ -1,50 +1,14 @@
 package main
 
 import (
-	"GOTR/03-algorithms/1-search"
-	"GOTR/GoSearch/pkg/crawler"
-	"GOTR/GoSearch/pkg/crawler/spider"
-	"GOTR/GoSearch/pkg/index"
-	"flag"
+	search "GOTR/03-algorithms/1-search"
 	"fmt"
 )
 
-// #TODO move search to 03-algorithms/1-search/search.go
 func main() {
 	sts := [2]string{
 		"https://go.dev/",
 		"https://www.programiz.com/golang/",
 	}
-	fmt.Println(ParseUrl(sts))
-}
-
-func ParseUrl(url [2]string) (docs []crawler.Document) {
-	var word string
-	var depth int
-	var BinaryDocs []crawler.Document
-
-	s := spider.New()
-	flag.StringVar(&word, "s", "", "word links")
-	flag.IntVar(&depth, "d", 1, "depth size")
-	flag.Parse()
-
-	for i := range url {
-		result, err := s.Scan(url[i], depth)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		index.Create(result[0])
-	}
-	if word != "" {
-		BinaryDocs = __search.Binary(index.MapIndex[word], index.Documents)
-	}
-
-	if BinaryDocs != nil {
-		docs = BinaryDocs
-	} else {
-		docs = index.Documents
-	}
-
-	return docs
+	fmt.Println(search.ParseUrl(sts))
 }
