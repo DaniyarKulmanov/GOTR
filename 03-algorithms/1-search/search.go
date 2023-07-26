a913dc3e479ac6304736aea9b3eb58b7616abe89
daniyar kulmanov <daniyarkulmanov@iMac.local>
2023-07-26 07:40:09 +0600

    refactor

--- 03-algorithms/1-search/search.go
+++ 03-algorithms/1-search/search.go
@@ -1,7 +1,7 @@
 package __search
 
 import (
-	"GO_thinknetica/GoSearch/pkg/crawler"
+	"GOTR/GoSearch/pkg/crawler"
 )
 
 func Binary(words []int, allDocs []crawler.Document) []crawler.Document {
--- GoSearch/pkg/index/index.go
+++ GoSearch/pkg/index/index.go
@@ -11,10 +11,9 @@ var Documents []crawler.Document
 
 func Create(data crawler.Document) {
 	words := strings.Fields(data.Title)
+	data.ID = len(Documents)
 	Documents = append(Documents, data)
-
 	for _, v := range words {
-		v = strings.ToLower(v)
 		MapIndex[v] = append(MapIndex[v], data.ID)
 	}
 	sort.Slice(Documents, func(i, j int) bool { return Documents[i].ID < Documents[j].ID })
