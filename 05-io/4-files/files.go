763aa5282d016eff63de6682db513d37833380e0
daniyar kulmanov <daniyarkulmanov@iMac.local>
2023-08-02 16:25:33 +0600

    recatored

--- 05-io/4-files/files.go
+++ 05-io/4-files/files.go
@@ -14,16 +14,18 @@ import (
 func main() {
 	var docs []crawler.Document
 	content, err := os.ReadFile("test.json")
-	if errors.Is(err, fs.ErrNotExist) {
-		docs = scanUrl()
-	} else {
+	if !errors.Is(err, fs.ErrNotExist) && err != nil {
+		log.Println(err)
+		return
+	}
+
+	if content == nil {
+		docs, err = scanUrl()
 		if err != nil {
 			log.Println(err)
 			return
 		}
-	}
-
-	if content != nil {
+	} else {
 		err = json.Unmarshal(content, &docs)
 		if err != nil {
 			log.Println(err)
@@ -33,13 +35,13 @@ func main() {
 	fmt.Println(docs)
 }
 
-func scanUrl() (data []crawler.Document) {
+func scanUrl() (data []crawler.Document, err error) {
 	sts := [2]string{
 		"https://go.dev/",
 		"https://www.programiz.com/golang/",
 	}
 	data = search.ParseUrl(sts)
 	file, _ := json.MarshalIndent(data, "", " ")
-	_ = os.WriteFile("test.json", file, 0644)
-	return data
+	err = os.WriteFile("test.json", file, 0644)
+	return data, err
 }
