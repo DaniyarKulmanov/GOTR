33e363cf076d89f2e3bdcae9d821dec5b545757a
daniyar kulmanov <daniyarkulmanov@iMac.local>
2023-08-02 09:52:01 +0600

    workable variant

--- 05-io/4-files/files.go
+++ 05-io/4-files/files.go
@@ -1,57 +1,45 @@
 package main
 
 import (
-	"GOTR/03-algorithms/1-search"
+	search "GOTR/03-algorithms/1-search"
 	"GOTR/GoSearch/pkg/crawler"
 	"encoding/json"
+	"errors"
 	"fmt"
-	"io"
-	"io/ioutil"
+	"io/fs"
 	"log"
 	"os"
 )
 
 func main() {
-	// scan data
+	var docs []crawler.Document
+	content, err := os.ReadFile("test.json")
+	if errors.Is(err, fs.ErrNotExist) {
+		docs = scanUrl()
+	} else {
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+
+	if content != nil {
+		err = json.Unmarshal(content, &docs)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+	fmt.Println(docs)
+}
+
+func scanUrl() (data []crawler.Document) {
 	sts := [2]string{
 		"https://go.dev/",
 		"https://www.programiz.com/golang/",
 	}
-	docs := search.ParseUrl(sts)
-	fmt.Println(docs)
-	// create file
-	f, err := os.Create("./scan.txt")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer f.Close()
-	//
-	//err = os.WriteFile(f.Name(), []byte("Текст"), 0666)
-	//if err != nil {
-	//	log.Println(err)
-	//	return
-	//}
-	// convert to json, save to file with json
-	file, _ := json.MarshalIndent(docs, "", " ")
-	_ = ioutil.WriteFile("test.json", file, 0644)
-	//readFile json()
-	content, err := ioutil.ReadFile("test.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("File contents: %s", content)
-	// unmarshal json to struct
-	var m []crawler.Document
-	err = json.Unmarshal(content, &m)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(m)
-}
-
-func saveFile(docs []crawler.Document, w io.Writer) error {
-	_, err := w.Write([]byte("Текст"))
-	fmt.Println(docs)
-	return err
+	data = search.ParseUrl(sts)
+	file, _ := json.MarshalIndent(data, "", " ")
+	_ = os.WriteFile("test.json", file, 0644)
+	return data
 }
