5781cf2e950d6d968b71d78f5e8af7e313c8320d
daniyar kulmanov <daniyarkulmanov@iMac.local>
2023-08-25 08:05:07 +0600

    hello world!

--- GoSearch/cmd/gosearch/gosearch.go
+++ GoSearch/cmd/gosearch/gosearch.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	search "GOTR/03-algorithms/1-search"
-	"fmt"
+	"GOTR/test_folder"
 )
 
 func main() {
-	sts := [2]string{
-		"https://go.dev/",
-		"https://www.programiz.com/golang/",
-	}
-	fmt.Println(search.ParseUrl(sts))
+	test_folder.Hello_w()
 }
